3cd4cc18437f75227ae47c06a8da84ad3ba2c7c4
Vsevolod Vaskin <vaskin.2003@gmail.com>
2024-10-24 17:12:53 +0300

    Points are visible in point_cloud_viewer

--- cartographer_example/configuration_files/assets_writer_ply.lua
+++ cartographer_example/configuration_files/assets_writer_ply.lua
@@ -5,15 +5,15 @@ include "transform.lua"
 options = {
     tracking_frame = "base_link",
     pipeline = {
-        -- {
-        --     action = "fixed_ratio_sampler",
-        --     sampling_ratio = 0.1, -- Retain only 10% of the points
-        -- },
-        -- {
-        --     action = "min_max_range_filter",
-        --     min_range = 1.0,  -- Minimum distance from the sensor
-        --     max_range = 60.0, -- Maximum distance from the sensor
-        -- },
+        {
+            action = "min_max_range_filter",
+            min_range = 1.0,  -- Minimum distance from the sensor
+            max_range = 60.0, -- Maximum distance from the sensor
+        },
+        {
+            action = "fixed_ratio_sampler",
+            sampling_ratio = 0.1, -- Retain only 10% of the points
+        },
         -- {
         --     action = "dump_num_points",
         -- },
