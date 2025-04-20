02acbc2e16973658224675758cf3be1e8af0205c
Vsevolod Vaskin <vaskin.2003@gmail.com>
2025-04-20 23:17:38 +0300

    Change mit_stata config for ceres optimization experiment

--- cartographer/configuration_files/mit_stata.lua
+++ cartographer/configuration_files/mit_stata.lua
@@ -49,7 +49,7 @@ options = {
 MAP_BUILDER.use_trajectory_builder_2d = true
 
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 25
-TRAJECTORY_BUILDER_2D.submaps.num_range_data = 1
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 10
 
 -- LOCAL SLAM
 -- TRAJECTORY_BUILDER_2D.min_range = 1 
