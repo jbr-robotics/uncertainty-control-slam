ef7e78ad84f3c7bde57481603bbc2bbabbfab2dd
Vsevolod Vaskin <vaskin.2003@gmail.com>
2025-05-12 22:33:20 +0300

    Conduct exeperiment: num_range_data_selection_4

---
+++ cartographer/experiments/2025-05-12_21-35-25_num_range_data_selection_4/README.md
@@ -0,0 +1,17 @@
+# Experiment: num_range_data_selection_4
+
+COMMIT: 3d950f02753d912403859bf18b8c5b2a71aa814a
+
+## Overview
+
+Rerun `2025-05-12_16-57-56_num_range_data_selection_3` with increased number of samples to improve robustness.
+
+## Results
+
+65 selected as it demonstrates "good" values across all metrics.
+
+![](image.png)
+
+![](image-1.png)
+
+![](image-2.png)
---
+++ cartographer/experiments/2025-05-12_21-35-25_num_range_data_selection_4/run_experiment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script must be run inside docker container. 
+# It contains the sequence of commands that must be executed to run the experiment.
+
+submap-grid-search \
+    --bag_filename /data/bags/2011-01-28-06-37-23 \
+    --config_dir /opt/ros/humble/share/cartographer_ros/configuration_files \
+    --config_basename mit_stata.lua \
+    --rate 1 \
+    --samples 10 \
+    --output_root /data/maps/num_range_data_4 \
+    --grid='{"trajectory_builder.trajectory_builder_2d.submaps.num_range_data": 
+                [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]}' \
+    --skip_begin 50 \
+    --skip_end 60 \
+    2>&1 | tee /data/num_range_data_selection_4.log
